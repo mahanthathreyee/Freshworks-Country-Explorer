3fcda3f7f085588db2f5f7b558cac389f5e951a7
Maganth Seetharaman <mahanthathreyee@gmail.com>
2019-07-25 07:10:56 +0530

    JavaScript function for display country detail written

--- js/AsyncHandler.js
+++ js/AsyncHandler.js
@@ -1,5 +1,5 @@
-function searchCountries(SearchString, DisplayFunction, ErrorHandlingFunction){
-    const CountryAPI_URL = new Request("https://restcountries.eu/rest/v2/name/" + SearchString);
+function searchCountries(SearchString, DisplayFunction, ErrorHandlingFunction, FullText = false){
+    const CountryAPI_URL = new Request("https://restcountries.eu/rest/v2/name/" + SearchString + (FullText ? "?fullText=true" : ""));
     
     fetch(CountryAPI_URL).then((response) => {
         if(response.status === 200)
--- js/Country.js
+++ js/Country.js
@@ -8,8 +8,47 @@ function getParameterByName(name) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
-    let CountrySelected = getParameterByName('country');
+    let CountrySelected = getParameterByName("country");
     //Todo Handle Invalid Data
-    
+    searchCountries(CountrySelected, displayCountryDetails, displayError, true);
 });
+
+function displayCountryDetails(Response){
+    console.log(Response);
+    document.title = Response[0]["name"];
+
+    let CountryFlag = document.getElementById("Country-Flag");
+    CountryFlag.setAttribute("src", Response[0]["flag"]);
+    
+    let CountryName = document.getElementById("Country-Name");
+    CountryName.innerHTML = Response[0]["name"];
+
+    const CountryTBody = document.getElementById('Country-TBody');
+    for(Key in Response[0]){
+        let TableRecord = document.createElement("tr");
+        TableRecord.className = "Country-Detail";
+        
+        let Detail_1 = document.createElement("td");
+        Detail_1.innerHTML = capitalizeFirstLetter(Key);
+        TableRecord.appendChild(Detail_1);
+
+        let Detail_2 = document.createElement("td");
+        if(typeof(Response[0][Key]) === "object")
+            Detail_2.innerHTML = JSON.stringify(Response[0][Key]);
+        else
+            Detail_2.innerHTML = Response[0][Key]
+        TableRecord.appendChild(Detail_2);
+
+        CountryTBody.appendChild(TableRecord);
+    }
+}
+
+function displayError(){
+    //Todo display error
+    console.log("Error");
+}
